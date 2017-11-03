b348c8301312ae1b66822846290f974815560e1c
Huáng Jùnliàng <i@jhuang.me>
2017-11-03 12:34:54 +0800

    feat(index): add more assertion

--- index.js
+++ index.js
@@ -11,4 +11,18 @@ describe("fs.statSync", () => {
             callback();
         })
     })
+    it("should return dev >= 0", () => {
+        fs.statSync(__filename).dev.should.be.above(0);
+    })
 });
+describe("fs.stat", () => {
+    it("should return dev >= 0", (callback) => {
+        fs.stat(__filename, (err, stat) => {
+            if (err !== null) {
+                throw err;
+            }
+            stat.dev.should.be.above(0);
+            callback();
+        })
+    })
+})
